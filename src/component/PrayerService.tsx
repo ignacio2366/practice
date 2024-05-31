7ccc77d84f73dac42daacf8319bfc324101e84ce
Mark Angelo Ignacio <76676425+ignacio2366@users.noreply.github.com>
2024-05-31 23:12:00 +0800

    eslint added

--- src/component/PrayerService.tsx
+++ src/component/PrayerService.tsx
@@ -1,12 +1,41 @@
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native"
-import styled from "../assets/styled"
-import Imaged from "../Image/images"
-const PrayerService = () => {
-    return (
-        <View style={_style.container}>
+import {
+  View, Text, StyleSheet, TouchableOpacity,
+} from 'react-native';
+import styled from '../assets/styled';
+import Imaged from '../Image/images.ts';
+
+const style = StyleSheet.create({
+  container: {
+    height: 78,
+    flexDirection: 'row',
+    backgroundColor: 'rgba(229, 239, 236, 0.1)',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 16,
+    borderWidth: 1,
+    borderColor: 'rgba(255, 255, 255, 0.1)',
+    borderRadius: 4,
+    marginVertical: 25,
+  },
+  lbl: {
+    color: styled.white,
+    fontSize: styled.small,
+    fontFamily: styled.normal,
+    textTransform: 'capitalize',
+  },
+  title: {
+    color: styled.white,
+    fontSize: styled.f18,
+    fontFamily: styled.sembold,
+    textTransform: 'capitalize',
+  },
+
+});
+const PrayerService = () => (
+        <View style={style.container}>
             <View style={{ gap: 4 }}>
-                <Text style={_style.lbl}>Daily Prayer</Text>
-                <Text style={_style.title}>One true Good</Text>
+                <Text style={style.lbl}>Daily Prayer</Text>
+                <Text style={style.title}>One true Good</Text>
             </View>
             <View>
                 <TouchableOpacity>
@@ -14,34 +43,6 @@ const PrayerService = () => {
                 </TouchableOpacity>
             </View>
         </View>
-    )
-}
+);
 
-const _style = StyleSheet.create({
-    container: {
-        height: 78,
-        flexDirection: 'row',
-        backgroundColor: 'rgba(229, 239, 236, 0.1)',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        paddingHorizontal: 16,
-        borderWidth: 1,
-        borderColor: 'rgba(255, 255, 255, 0.1)',
-        borderRadius: 4,
-        marginVertical: 25,
-    },
-    lbl: {
-        color: styled.white,
-        fontSize: styled.small,
-        fontFamily: styled.normal,
-        textTransform: 'capitalize'
-    },
-    title: {
-        color: styled.white,
-        fontSize: styled.f18,
-        fontFamily: styled.sembold,
-        textTransform: 'capitalize'
-    }
-
-})
-export default PrayerService
+export default PrayerService;
--- src/screen/HomeScreen.tsx
+++ src/screen/HomeScreen.tsx
@@ -1,25 +1,27 @@
-import { useEffect, useState } from "react";
-import { DeviceEventEmitter, Text, ToastAndroid, View } from "react-native";
+import { useEffect, useState } from 'react';
+import {
+  DeviceEventEmitter, Text, ToastAndroid, View,
+} from 'react-native';
 
 const HomeScreen = () => {
-    const [intentReceived, setIntentReceived] = useState(false);
+  const [intentReceived, setIntentReceived] = useState(false);
 
-    const cameraSurvey = async () => {
-        ToastAndroid.show("CameraSurvey", ToastAndroid.TOP);
-        setIntentReceived(true);
-    };
+  const cameraSurvey = async () => {
+    ToastAndroid.show('CameraSurvey', ToastAndroid.TOP);
+    setIntentReceived(true);
+  };
 
-    useEffect(() => {
-        DeviceEventEmitter.addListener('onShortcutPressed', cameraSurvey);
-    }, [intentReceived]);
+  useEffect(() => {
+    DeviceEventEmitter.addListener('onShortcutPressed', cameraSurvey);
+  }, [intentReceived]);
 
-    return (
+  return (
         <View>
             <Text style={{ color: 'black' }}>Home SAD</Text>
             {intentReceived && <Text style={{ color: 'black' }}>SAD</Text>}
 
         </View>
-    );
+  );
 };
 
 export default HomeScreen;
